5f4b82fadc4f1dbb99fd2c71c1252734486ff59f
Dmitrij Kolotusha <suvstreet@gmail.com>
2024-01-27 20:40:22 +0300

    add container with text  urge to do something

--- src/app.js
+++ src/app.js
@@ -2,4 +2,19 @@ import './styles.css'
 
 import { ContextMenu } from './menu'
 
-new ContextMenu('body').open()
+class App {
+  constructor() {
+    new ContextMenu('body').open()
+
+    this.body = document.querySelector('body')
+    this.container = document.createElement('div')
+    this.container.classList.add('container')
+    this.container.innerHTML = 'Нажми на меня правой кнопкой мыши'
+  }
+
+  render() {
+    this.body.append(this.container)
+  }
+}
+
+new App().render()
