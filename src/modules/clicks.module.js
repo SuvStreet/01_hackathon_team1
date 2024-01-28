69396277343465ee1702d092cfbaa2b9f61bce36
Dmitrij Kolotusha <suvstreet@gmail.com>
2024-01-28 23:25:26 +0300

    finally module/clicks

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,73 +1,71 @@
 import { Module } from '../core/module'
 
 export class ClicksModule extends Module {
-	constructor(type, text) {
-		super(type, text)
+  constructor(type, text) {
+    super(type, text)
 
-		this.clicks = null
+    this.clicks = null
 
-		this.oneClick = 0
-		this.dblClick = 0
-	}
+    this.oneClick = 0
+    this.dblClick = 0
+  }
 
-	trigger() {
-		this.hideContainer()
-		this.usersInteractiv()
-	}
+  trigger() {
+    this.hideContainer()
+    this.usersInteractiv()
+  }
 
-	hideContainer() {
-		this.clicks = document.querySelector('.clicks')
-		this.clicks.innerHTML = ''
-	}
+  hideContainer() {
+    this.clicks = document.querySelector('.clicks')
+    this.clicks.innerHTML = ''
+  }
 
-	usersInteractiv() {
-		const clicksContainer = document.createElement('div')
-		clicksContainer.classList.add('clicksContainer')
-		clicksContainer.textContent = 'Кликай быстрее'
-		this.clicks.append(clicksContainer)
+  usersInteractiv() {
+    const clicksContainer = document.createElement('div')
 
-      this.counter(clicksContainer)
-		
-      
-	}
+    clicksContainer.classList.add('clicksContainer')
+    clicksContainer.textContent = 'Кликай быстрее'
+    this.clicks.append(clicksContainer)
+    this.counter(clicksContainer)
+  }
 
-	counter(clicksContainer) {
-		clicksContainer.addEventListener('click', event => {
-			if (event.detail === 1) {
-				this.oneClick += 1
-			} else {
-				this.oneClick -= 1
-				this.dblClick += 1
-			}
-		})
+  counter(clicksContainer) {
+    clicksContainer.addEventListener('click', (event) => {
+      if (event.detail === 1) {
+        this.oneClick += 1
+      } else {
+        this.oneClick -= 1
+        this.dblClick += 1
+      }
+    })
 
-		setTimeout(() => {
-			clicksContainer.remove()
-			this.render()
+    setTimeout(() => {
+      clicksContainer.remove()
+      this.render()
 
-			setTimeout(() => {
-				this.oneClick = 0
-				this.dblClick = 0
-				this.clicks.innerHTML = 'Нажми на меня правой кнопкой мыши'
-				this.clicks.classList.remove('clicks')
-			}, 5000)
-		}, 3000)
-	}
+      setTimeout(() => {
+        this.oneClick = 0
+        this.dblClick = 0
+        this.clicks.innerHTML = 'Нажми на меня правой кнопкой мыши'
+        this.clicks.classList.remove('clicks')
+      }, 5000)
+    }, 3000)
+  }
 
-	render() {
-		const titleModal = document.createElement('div')
-		titleModal.className = 'modalActive_item'
-		titleModal.textContent = 'Колличество Ваших кликов:'
+  render() {
+    const titleModal = document.createElement('div')
+    titleModal.className = 'modalActive_item'
+    titleModal.textContent = 'Колличество Ваших кликов:'
 
-		const itemOneLeftClicks = document.createElement('p')
-		itemOneLeftClicks.className = 'modalActive_list'
-		itemOneLeftClicks.textContent = `Количество одиночных кликов: ${this.oneClick}`
+    const itemOneLeftClicks = document.createElement('p')
+    itemOneLeftClicks.className = 'modalActive_list'
+    itemOneLeftClicks.textContent = `Количество одиночных кликов: ${this.oneClick}`
 
-		const itemTwoLeftClicks = document.createElement('p')
-		itemTwoLeftClicks.className = 'modalActive_list'
-		itemTwoLeftClicks.textContent = `Количество двойных кликов: ${this.dblClick}`
+    const itemTwoLeftClicks = document.createElement('p')
+    itemTwoLeftClicks.className = 'modalActive_list'
+    itemTwoLeftClicks.textContent = `Количество двойных кликов: ${this.dblClick}`
 
-		titleModal.append(itemOneLeftClicks, itemTwoLeftClicks)
-		this.clicks.append(titleModal)
-	}
+    titleModal.append(itemOneLeftClicks, itemTwoLeftClicks)
+    this.clicks.append(titleModal)
+  }
 }
