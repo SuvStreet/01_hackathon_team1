1a64ac15ea74bd9aaa7d99a52b62823b6ae47475
Dmitrij Kolotusha <suvstreet@gmail.com>
2024-01-28 18:52:38 +0300

    modification module.clicks

--- src/menu.js
+++ src/menu.js
@@ -7,43 +7,52 @@ import { SoundModule } from './modules/sound.module'
 import { TimerModule } from './modules/timer.module'
 
 const ArrayOfModules = [
-	new BackgroundModule('background', 'Случайный фон'),
-	new ClicksModule('clicks', 'Аналитика кликов (3 секунды)'),
-	new MessageModule('message', 'Сообщения'),
-	new ShapeModule('shape', 'Фигуры'),
-	new SoundModule('sound', 'Звуки'),
-	new TimerModule('timer', 'Таймер')
+  new BackgroundModule('background', 'Случайный фон'),
+  new ClicksModule('clicks', 'Аналитика кликов (3 секунды)'),
+  new MessageModule('message', 'Сообщения'),
+  new ShapeModule('shape', 'Фигуры'),
+  new SoundModule('sound', 'Звуки'),
+  new TimerModule('timer', 'Таймер'),
 ]
 
 export class ContextMenu extends Menu {
-	constructor(selector) {
-		super(selector)
-		this.menu = document.querySelector('#menu')
-	}
+  constructor(selector) {
+    super(selector)
+    this.menu = document.querySelector('#menu')
+  }
 
-	open() {
-		this.el.addEventListener('contextmenu', event => {
-			event.preventDefault()
-			this.menu.style.top = `${event.clientY}px`
-			this.menu.style.left = `${event.clientX}px`
-			this.add()
-		})
-
-		this.menu.addEventListener('click', event => {
-			event.stopPropagation()
-			ArrayOfModules.find(
-				module => module.type === event.target.dataset.type
-			).trigger()
+  open() {
+    this.el.addEventListener('contextmenu', (event) => {
+      event.preventDefault()
+      this.menu.style.top = `${event.clientY}px`
+      this.menu.style.left = `${event.clientX}px`
+      this.add()
 			this.close()
-		})
-	}
+    })
 
-	close() {
-		this.menu.classList.remove('open')
-	}
+    this.menu.addEventListener('click', (event) => {
+      event.stopPropagation()
+      document
+        .querySelector('.container')
+        .classList.add(`${event.target.dataset.type}`)
+      ArrayOfModules.find(
+        (module) => module.type === event.target.dataset.type
+      ).trigger()
+      this.close()
+			this.menu.classList.remove('open')
+    })
+  }
 
-	add() {
-		this.menu.classList.add('open')
-		this.menu.innerHTML = ArrayOfModules.map(module => module.toHTML()).join('')
-	}
+  close() {
+		this.el.addEventListener('click', () => {
+      this.menu.classList.remove('open')
+    })
+  }
+
+  add() {
+    this.menu.classList.add('open')
+    this.menu.innerHTML = ArrayOfModules.map((module) => module.toHTML()).join(
+      ''
+    )
+  }
 }
--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -4,34 +4,44 @@ export class ClicksModule extends Module {
   constructor(type, text) {
     super(type, text)
 
+    this.clicks = null
+
     this.oneClick = 0
     this.dblClick = 0
   }
 
   trigger() {
-    this.counter()
+    this.clicks = document.querySelector('.clicks')
+    this.clicks.innerHTML = ''
+
+    const clicksContainer = document.createElement('div')
+    clicksContainer.classList.add('clicksContainer')
+    clicksContainer.textContent = 'Кликай быстрее'
+    this.clicks.append(clicksContainer)
+
+    this.counter(clicksContainer)
   }
 
-  counter() {
-    document.body.addEventListener('click', (event) => {
+  counter(clicksContainer) {
+    clicksContainer.addEventListener('click', (event) => {
       if(event.detail === 1) {
         this.oneClick += 1
-      }
-
-      // if(event.button === 2) {
-      //   console.log('event.button === 2')
-      //   document.querySelector('#menu').classList.toggle('open')
-      // }
-    })
-
-    document.body.addEventListener('dblclick', (event) => {
+      } else {
         this.oneClick -= 1
         this.dblClick += 1
       }
-    )
-    
+    })
+
     setTimeout(() => {
+      clicksContainer.remove()
       this.render()
+
+      setTimeout(() => {
+        this.oneClick = 0
+        this.dblClick = 0
+        this.clicks.innerHTML = 'Нажми на меня правой кнопкой мыши'
+        this.clicks.classList.remove('clicks')
+      }, 5000)
     }, 3000)
   }
 
@@ -49,6 +59,6 @@ export class ClicksModule extends Module {
     itemTwoLeftClicks.textContent = `Количество двойных кликов: ${this.dblClick}`
 
     titleModal.append(itemOneLeftClicks, itemTwoLeftClicks)
-    document.body.append(titleModal)
+    this.clicks.append(titleModal)
   }
 }
